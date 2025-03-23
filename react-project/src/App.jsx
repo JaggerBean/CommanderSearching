5cfa91144a40a1359568be9f070ff57d005ba630
Hriju Shreshtha <49452078+Hriju-Shreshtha@users.noreply.github.com>
2025-03-23 16:44:17 +0000

    Hriju Course Project 0405e

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,18 +1,55 @@
 import "./App.css";
-
-function Header(props) {
+import { useState, useReducer } from "react";
+function Header({ name, year, course }) {
   return (
     <header>
-      <h1>{props.name}'s Kitchen</h1>
-      <p>Copyright {props.year}</p>
+      <h1>
+        {name}'s {course} Learning
+      </h1>
+      <p>Copyright {year}</p>
     </header>
+  );
+}
+const items = ["Beginner", "Intermediate", "Advanced"];
+
+const levelsObjects = items.map((item, i) => ({
+  id: i,
+  value: item,
+  count: 0,
+}));
+
+function Main({ levels }) {
+  return (
+    <main>
+      <img
+        src="https://github.com/Hriju-Shreshtha.png"
+        height={200}
+        alt="food"
+      />
+      <ul>
+        {levels.map((item) => (
+          <li key={item.id} style={{ listStyleType: "none" }}>
+            {item.value}
+            {item.count}
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
 
 function App() {
+  // const [status, setStatus] = useState(true);
+  const [status, toggleStatus] = useReducer((status)=>!status, true);
   return (
     <div>
-      <Header name="Alex" year={new Date().getFullYear()} />
+      <h1>The course is {status ? "Available" : "Closed"}.</h1>
+      <button onClick={toggleStatus}>
+        {" "}
+        Course is {status ? "Full" : "Available"}
+      </button>
+      <Header name="Hriju" year={new Date().getFullYear()} course="React" />
+      <Main levels={levelsObjects} openStatus={status} onStatus={toggleStatus} />
       <main>
         <h2>We serve the most delicious food around</h2>
       </main>
