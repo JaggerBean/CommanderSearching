863d6cfbff85ad5cb94ee22e9a10c11e0f7a5e03
Samantha Harper <149115727+Sharper93@users.noreply.github.com>
2025-04-15 16:55:52 +0000

    Updates to app.jsx, package.json, and main.jsx

---
+++ react-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+let language = "JavaScript";
+let moon = "🌙";
+
+function App() {
+  return (
+    <h1>Hello {language.toUpperCase()} {moon}!</h1>
+
+  )
+}
+
+export default App
